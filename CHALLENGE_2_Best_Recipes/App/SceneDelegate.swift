4d1f92b1627541dec3e005e5c0f226ad9fa61ff4
Danila Bolshakov <danila1999777@gmail.com>
2023-08-29 09:23:30 +0300

    Update Project
    The project has been corrected put #warrning( in SceneDelegate and Added README.md file + SeeAllViewController

--- CHALLENGE_2_Best_Recipes/App/SceneDelegate.swift
+++ CHALLENGE_2_Best_Recipes/App/SceneDelegate.swift
@@ -14,7 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowSene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowSene)
-        let welcomeVC = UINavigationController(rootViewController: RecipeDetailViewController())
+        #warning("Update UINavigationController(rootViewController: RecipeViewController())")
+        let welcomeVC = UINavigationController(rootViewController: RecipeViewController())
         window?.rootViewController = welcomeVC
         window?.makeKeyAndVisible()
         
