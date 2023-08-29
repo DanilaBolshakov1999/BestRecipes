bf10b250daace0401a36933d028f1a1ed7193707
Danila Bolshakov <danila1999777@gmail.com>
2023-08-30 00:58:22 +0300

    Update Danila/RecipeDetaliViewController
    Scroll View setup process

---
+++ CHALLENGE_2_Best_Recipes/Extension/UIButton + Extention.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  CHALLENGE_2_Best_Recipes
+//
+//  Created by iOS - Developer on 29.08.2023.
+//
+
+import Foundation
+
+extension UIButton {
+    convenience init(titleColor: UIColor?, backgroundColor: UIColor? = .clear, cornerRadius: CGFloat) {
+        self.init(type: .system)
+        self.titleLabel?.font = .systemFont(ofSize: 30)
+        self.setTitleColor(titleColor, for: .normal)
+        self.backgroundColor = backgroundColor
+        self.layer.cornerRadius = cornerRadius
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ CHALLENGE_2_Best_Recipes/Extension/UILabel + Extention.swift
@@ -0,0 +1,8 @@
+//
+//  UILabel + Extention.swift
+//  CHALLENGE_2_Best_Recipes
+//
+//  Created by iOS - Developer on 30.08.2023.
+//
+
+import Foundation
