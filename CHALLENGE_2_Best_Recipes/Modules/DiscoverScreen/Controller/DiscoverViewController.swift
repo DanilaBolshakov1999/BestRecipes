88c003a2a4d1da728b2bb2d7445e3b4586d6822b
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-08-30 17:19:44 +0500

    Created DicoverVC

---
+++ CHALLENGE_2_Best_Recipes/Modules/DiscoverScreen/Controller/DiscoverViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DiscoverViewController.swift
+//  CHALLENGE_2_Best_Recipes
+//
+//  Created by Ислам Пулатов on 8/30/23.
+//
+
+import UIKit
+import SnapKit
+
+final class DiscoverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
