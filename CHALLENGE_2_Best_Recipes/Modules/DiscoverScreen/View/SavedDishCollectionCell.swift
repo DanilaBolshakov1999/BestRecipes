1f9f1e54f440a705ef389fed81fd8af7356557d1
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-08-30 19:24:09 +0500

    Modified CustonCell
    Separated UI Logic

--- CHALLENGE_2_Best_Recipes/Modules/DiscoverScreen/View/SavedDishCollectionCell.swift
+++ CHALLENGE_2_Best_Recipes/Modules/DiscoverScreen/View/SavedDishCollectionCell.swift
@@ -9,113 +9,102 @@ import UIKit
 
 class SavedDishCollectionCell: UICollectionViewCell {
     
-    private let dishImage: UIImageView = {
-        let img = UIImageView()
-        
-        img.contentMode = .scaleAspectFill
-        img.clipsToBounds = true
-        img.layer.cornerRadius = 10
-        
-        return img
+    private let dishBackGroundImage: UIImageView = {
+        let dishBackGroundImage = UIImageView()
+        dishBackGroundImage.contentMode = .scaleAspectFill
+        dishBackGroundImage.clipsToBounds = true
+        dishBackGroundImage.layer.cornerRadius = 10
+        return dishBackGroundImage
     }()
     
-    private let ratingStack: UIStackView = {
-        let stck = UIStackView()
-        
-        stck.distribution = .fillEqually
-        stck.axis = .horizontal
-        stck.spacing = 2
-        
-        return stck
+    private let ratingOfDishStack: UIStackView = {
+        let ratingOfDishStack = UIStackView()
+        ratingOfDishStack.distribution = .fillEqually
+        ratingOfDishStack.axis = .horizontal
+        ratingOfDishStack.spacing = 2
+        return ratingOfDishStack
     }()
     
     private let starImageView: UIImageView = {
-        let imageView = UIImageView(image: UIImage(systemName: "star.fill"))
-        
-        imageView.tintColor = .purpleText
-        imageView.contentMode = .scaleAspectFit
-        
-        return imageView
+        let starImageView = UIImageView(image: UIImage(systemName: "star.fill"))
+        starImageView.tintColor = .black
+        starImageView.contentMode = .scaleAspectFit
+        return starImageView
     }()
     
     private let ratingMarkLabel: UILabel = {
-        let lbl = UILabel()
-        
-        lbl.text = ""
-        lbl.textColor = .purpleText
-        
-        lbl.font = UIFont(name: "Poppins", size: 20)
-        
-        lbl.numberOfLines = 0
-        lbl.lineBreakMode = .byWordWrapping
-        lbl.textAlignment = .left
-        
-        return lbl
+        let ratingMarkLabel = UILabel()
+        ratingMarkLabel.text = ""
+        ratingMarkLabel.textColor = .purpleText
+        ratingMarkLabel.font = UIFont(name: "Poppins", size: 20)
+        ratingMarkLabel.numberOfLines = 0
+        ratingMarkLabel.lineBreakMode = .byWordWrapping
+        ratingMarkLabel.textAlignment = .left
+        return ratingMarkLabel
     }()
     
     private let saveButton: UIButton = {
-        let btn = UIButton(type: .system)
-        
-        btn.backgroundColor = .white
-        btn.layer.cornerRadius = btn.frame.height / 2
-        btn.clipsToBounds = true
-        
-        let image = UIImage(named: "Favorites")
-        btn.setImage(image, for: .normal)
-        btn.tintColor = .gray
-        
-        return btn
+        let saveButton = UIButton(type: .system)
+        saveButton.backgroundColor = .white
+        saveButton.layer.cornerRadius = saveButton.frame.height / 2
+        saveButton.clipsToBounds = true
+        let image = UIImage(named: Theme.favorite)
+        saveButton.setImage(image, for: .normal)
+        saveButton.tintColor = .red
+        return saveButton
     }()
     
     private let dishTitle: UILabel = {
-        let lbl = UILabel()
-        
-        lbl.font = UIFont(name: "Poppins", size: 20)
-        lbl.textColor = .purpleText
-        lbl.numberOfLines = 0
-        lbl.lineBreakMode = .byWordWrapping
-        lbl.textColor = .purpleText
-        
-        return lbl
+        let dishTitle = UILabel()
+        dishTitle.font = UIFont(name: Theme.Fonts.appFont, size: 16)
+        dishTitle.textColor = .purpleText
+        dishTitle.numberOfLines = 0
+        dishTitle.lineBreakMode = .byWordWrapping
+        dishTitle.textColor = .black
+        return dishTitle
     }()
     
     public func configure(with image: UIImage, title: String, rating: String) {
-        dishImage.image = image
+        dishBackGroundImage.image = image
         dishTitle.text = title
         ratingMarkLabel.text = rating
-        
-        ratingStack.addArrangedSubview(starImageView)
-        ratingStack.addArrangedSubview(ratingMarkLabel)
+        ratingOfDishStack.addArrangedSubview(starImageView)
+        ratingOfDishStack.addArrangedSubview(ratingMarkLabel)
         
         setUpCell()
     }
     
+    
+}
+
+extension SavedDishCollectionCell {
+    
     private func setUpCell() {
-        addSubview(dishImage)
-        dishImage.snp.makeConstraints { make in
+        addCellSubViews()
+        setUpConstrains()
+    }
+    
+    private func addCellSubViews() {
+        addSubview(dishBackGroundImage)
+        addSubview(ratingOfDishStack)
+        addSubview(saveButton)
+        addSubview(dishTitle)
+    }
+    
+    private func setUpConstrains() {
+        dishBackGroundImage.snp.makeConstraints { make in
             make.trailing.leading.top.equalToSuperview()
             make.bottom.equalToSuperview().inset(50)
         }
         
-        let blurEffect = UIBlurEffect(style: .light)
-        let blurView = UIVisualEffectView(effect: blurEffect)
-        blurView.frame = ratingStack.bounds
-        blurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        blurView.layer.cornerRadius = 5
-        blurView.clipsToBounds = true
-        
-        ratingStack.insertSubview(blurView, at: 0)
-        ratingStack.backgroundColor = .clear
-        
-        addSubview(ratingStack)
-        ratingStack.snp.makeConstraints { make in
+        ratingOfDishStack.snp.makeConstraints { make in
             make.top.equalToSuperview().inset(10)
             make.leading.equalToSuperview().inset(20)
             make.height.equalTo(27.5)
             make.width.equalTo(70)
         }
+        applyBlurEffect(to: ratingOfDishStack)
         
-        addSubview(saveButton)
         saveButton.snp.makeConstraints { make in
             make.top.equalToSuperview().inset(10)
             make.trailing.equalToSuperview().inset(20)
@@ -125,10 +114,21 @@ class SavedDishCollectionCell: UICollectionViewCell {
         saveButton.layoutIfNeeded()
         saveButton.layer.cornerRadius = saveButton.frame.height / 2
         
-        addSubview(dishTitle)
         dishTitle.snp.makeConstraints { make in
-            make.top.equalTo(dishImage.snp.bottom).offset(10)
+            make.top.equalTo(dishBackGroundImage.snp.bottom).offset(10)
             make.leading.trailing.equalToSuperview()
         }
     }
+    
+    private func applyBlurEffect(to view: UIView) {
+        let blurEffect = UIBlurEffect(style: .light)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.frame = view.bounds
+        blurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        blurView.layer.cornerRadius = 5
+        blurView.clipsToBounds = true
+        view.insertSubview(blurView, at: 0)
+        view.backgroundColor = .clear
+    }
+    
 }
