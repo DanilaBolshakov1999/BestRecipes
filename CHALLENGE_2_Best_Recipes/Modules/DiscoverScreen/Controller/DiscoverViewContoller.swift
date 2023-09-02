3bcc1150b1ee19740a3828bdf1de400db6338350
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-09-02 16:50:58 +0500

    Created DiscoverViewContoller
    Created SavedDishCollectionCell

---
+++ CHALLENGE_2_Best_Recipes/Modules/DiscoverScreen/Controller/DiscoverViewContoller.swift
@@ -0,0 +1,19 @@
+//
+//  DiscoverViewContoller.swift
+//  CHALLENGE_2_Best_Recipes
+//
+//  Created by Ислам Пулатов on 9/2/23.
+//
+
+import UIKit
+import SnapKit
+
+final class DiscoverViewContoller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
---
+++ CHALLENGE_2_Best_Recipes/Modules/DiscoverScreen/View/SavedDishCollectionCell.swift
@@ -0,0 +1,12 @@
+//
+//  SavedDishCollectionCell.swift
+//  CHALLENGE_2_Best_Recipes
+//
+//  Created by Ислам Пулатов on 9/2/23.
+//
+
+import UIKit
+
+class SavedDishCollectionCell: UICollectionViewCell {
+    
+}
