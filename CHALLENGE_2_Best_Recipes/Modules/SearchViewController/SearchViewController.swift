7684b0762225cebd1c99eff8f4b1da18ca33371a
Danila Bolshakov <danila1999777@gmail.com>
2023-09-09 18:53:39 +0300

    Initial Commit

---
+++ CHALLENGE_2_Best_Recipes/Modules/SearchViewController/SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewController.swift
+//  CHALLENGE_2_Best_Recipes
+//
+//  Created by iOS - Developer on 09.09.2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
